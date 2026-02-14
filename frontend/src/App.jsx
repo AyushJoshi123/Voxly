69078259a5391063f425ec5e70f04f880c1d9c73
AyushJoshi123 <joshiayush00712345@gmail.com>
2026-02-14 07:09:43 +0530

    Initial commit: frontend setup with Chakra UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react"
+
+function App() {
+  return (
+    <Button colorScheme="cyan" size="lg">
+      Chakra Working 🚀
+    </Button>
+  )
+}
+
+export default App
